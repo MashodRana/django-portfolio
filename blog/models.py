f66f332647bc2a9370b817028476e34a848a9a4f
MashodRana <mashod0rana@gmail.com>
2022-11-16 17:39:03 +0600

    model of blogs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class CategoryModel(models.Model):
+    """Store category of post."""
+    name = models.CharField(max_length=31)
+
+
+class PostModel(models.Model):
+    """ Store post details information """
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(CategoryModel, related_name='posts')
+
+
+class CommentModel(models.Model):
+    author = models.CharField(max_length=63)
