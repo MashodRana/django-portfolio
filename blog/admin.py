17b48b025fb0d6273aa7edc520188971343adbc2
MashodRana <mashod0rana@gmail.com>
2022-11-17 11:28:46 +0600

    blog models registered to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from blog.models import PostModel, CategoryModel
+
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(PostModel, PostAdmin)
+admin.site.register(CategoryModel, CategoryAdmin)
