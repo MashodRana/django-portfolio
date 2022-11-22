80ff6a1ced779ff711cb508a0ea7602fdf1b9076
MashodRana <mashod0rana@gmail.com>
2022-11-22 11:22:44 +0600

    registration done

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ from users import views
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard/', views.dashboard, name='user_dashboard'),
+    path('registration/', views.register, name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.urls import reverse
+
+from users.froms import CustomUserCreationForm
+
 
 # Create your views here.
 def dashboard(request):
     return render(request, template_name='users/dashboard.html')
+
+def register(request):
+    if request.method=="POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+        return redirect(reverse('user_dashboard'))
+        
+    elif request.method=='GET':
+        context = {
+            'form': CustomUserCreationForm()
+        }
+        return render(request, template_name='registration/register.html', context=context)
